c21ba8ff78e4d88381d65acfd68c3d248b2724ef
Rich Garcia <rchgrca@yahoo.com>
2017-08-20 15:25:15 -0700

    adds email sorting and date formatting

--- src/routes/Email/components/Email.js
+++ src/routes/Email/components/Email.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import moment from 'moment'
 import MailRead from './MailRead'
 import MailUnread from './MailUnread'
 import './EmailView.scss'
+
+const keysrt = (key) => {
+  return function(a,b){
+    if (a[key] > b[key]) return 1;
+    if (a[key] < b[key]) return -1;
+    return 0;
+  }
+}
+
+const convertDateToMs = (messages) => {
+  return messages.map((message) => {
+    return {
+      ...message,
+      ...message['date'] = parseInt(moment(message.date).format('x'), 10)
+     }
+  })
+}
+
+const convertDateToFormat = (messages) => {
+  return messages.map((message) => {
+    const isMessageToday = moment().format('MMM D') == moment(message.date).format('MMM D')
+    const sFormat = isMessageToday ? 'h mm A' : 'MMM D'
+    return {
+      ...message,
+      ...message['date'] = moment(message.date).format(sFormat)
+     }
+  })
+}
 
 const getMail = (messages, isUnread) => {
   return messages.filter((message) => {
@@ -11,11 +40,12 @@ const getMail = (messages, isUnread) => {
 }
 
 export const Email = ({ messages, markRead, markUnread }) => {
+  convertDateToMs(messages)
   return (
     <div className="containerEmail">
       <h2>Twine Email</h2>
-      <MailUnread messages={getMail(messages,true)} handleClick={markRead} />
-      <MailRead messages={getMail(messages,false)} handleClick={markUnread} />
+      <MailUnread messages={convertDateToFormat(getMail(messages,true).sort(keysrt('date')).reverse())} handleClick={markRead} />
+      <MailRead messages={convertDateToFormat(getMail(messages,false).sort(keysrt('date')).reverse())} handleClick={markUnread} />
     </div>
   )
 }
