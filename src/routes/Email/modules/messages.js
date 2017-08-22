9c9f9869007a2337ae334865403514dbcf70e6af
Rich Garcia <rchgrca@yahoo.com>
2017-08-21 18:46:27 -0700

    fixes lint errors

--- src/routes/Email/modules/messages.js
+++ src/routes/Email/modules/messages.js
@@ -1,108 +1,108 @@
 export default Object.assign({}, {
-  "messages": [
+  'messages': [
     {
-      "id": 0,
-      "subject": "Tahoe Trip Next Weekend",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 0,
+      'subject': 'Tahoe Trip Next Weekend',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "Mi augue mattis vitae erat risus nibh.",
-      "date": "2017-08-19T14:30Z",
-      "unread": true
+      'body': 'Mi augue mattis vitae erat risus nibh.',
+      'date': '2017-08-19T14:30Z',
+      'unread': true
     },
     {
-      "id": 1,
-      "subject": "You may have just won...",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 1,
+      'subject': 'You may have just won...',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "You may have just won the Illinois State Lottery!",
-      "date": "2017-08-19T04:30Z",
-      "unread": true
+      'body': 'You may have just won the Illinois State Lottery!',
+      'date': '2017-08-19T04:30Z',
+      'unread': true
     },
     {
-      "id": 2,
-      "subject": "You won't believe the top 10 best places to live",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 2,
+      'subject': 'You won\'t believe the top 10 best places to live',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "But you'll LOVE #3!",
-      "date": "2017-08-18T14:30Z",
-      "unread": false
+      'body': 'But you\'ll LOVE #3!',
+      'date': '2017-08-18T14:30Z',
+      'unread': false
     },
     {
-      "id": 3,
-      "subject": "Stop spam once and for all",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 3,
+      'subject': 'Stop spam once and for all',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "Only you can prevent spam",
-      "date": "2017-08-17T14:30Z",
-      "unread": false
+      'body': 'Only you can prevent spam',
+      'date': '2017-08-17T14:30Z',
+      'unread': false
     },
     {
-      "id": 4,
-      "subject": "Well hello there!",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 4,
+      'subject': 'Well hello there!',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "I promise I won't bother you again",
-      "date": "2017-08-17T14:30Z",
-      "unread": true
+      'body': 'I promise I won\'t bother you again',
+      'date': '2017-08-17T14:30Z',
+      'unread': true
     },
     {
-      "id": 5,
-      "subject": "Your confirmation is required",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 5,
+      'subject': 'Your confirmation is required',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "Please come to our site",
-      "date": "2017-08-17T14:30Z",
-      "unread": false
+      'body': 'Please come to our site',
+      'date': '2017-08-17T14:30Z',
+      'unread': false
     },
     {
-      "id": 6,
-      "subject": "Have you forgotten?",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 6,
+      'subject': 'Have you forgotten?',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "It's that time of year again...",
-      "date": "2017-08-20T14:30Z",
-      "unread": true
+      'body': 'It\'s that time of year again...',
+      'date': '2017-08-20T14:30Z',
+      'unread': true
     },
     {
-      "id": 7,
-      "subject": "Your account has expired",
-      "from": "x@gmail.com",
-      "to": [
-      "a@gmail.com",
-      "b@gmail.com",
-      "c.gmail.com"
+      'id': 7,
+      'subject': 'Your account has expired',
+      'from': 'x@gmail.com',
+      'to': [
+      'a@gmail.com',
+      'b@gmail.com',
+      'c.gmail.com'
       ],
-      "body": "Please verify",
-      "date": "2017-08-20T14:30Z",
-      "unread": false
+      'body': 'Please verify',
+      'date': '2017-08-20T14:30Z',
+      'unread': false
     },
   ]
 })
