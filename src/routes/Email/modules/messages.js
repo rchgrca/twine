d58e4bb2c8bd947caf0fd664563e83ce2c9318a6
Rich Garcia <rchgrca@yahoo.com>
2017-08-21 18:50:19 -0700

    fixes lint errors

--- src/routes/Email/modules/messages.js
+++ src/routes/Email/modules/messages.js
@@ -5,9 +5,9 @@ export default Object.assign({}, {
       'subject': 'Tahoe Trip Next Weekend',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'Mi augue mattis vitae erat risus nibh.',
       'date': '2017-08-19T14:30Z',
@@ -18,9 +18,9 @@ export default Object.assign({}, {
       'subject': 'You may have just won...',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'You may have just won the Illinois State Lottery!',
       'date': '2017-08-19T04:30Z',
@@ -31,9 +31,9 @@ export default Object.assign({}, {
       'subject': 'You won\'t believe the top 10 best places to live',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'But you\'ll LOVE #3!',
       'date': '2017-08-18T14:30Z',
@@ -44,9 +44,9 @@ export default Object.assign({}, {
       'subject': 'Stop spam once and for all',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'Only you can prevent spam',
       'date': '2017-08-17T14:30Z',
@@ -57,9 +57,9 @@ export default Object.assign({}, {
       'subject': 'Well hello there!',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'I promise I won\'t bother you again',
       'date': '2017-08-17T14:30Z',
@@ -70,9 +70,9 @@ export default Object.assign({}, {
       'subject': 'Your confirmation is required',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'Please come to our site',
       'date': '2017-08-17T14:30Z',
@@ -83,9 +83,9 @@ export default Object.assign({}, {
       'subject': 'Have you forgotten?',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'It\'s that time of year again...',
       'date': '2017-08-20T14:30Z',
@@ -96,9 +96,9 @@ export default Object.assign({}, {
       'subject': 'Your account has expired',
       'from': 'x@gmail.com',
       'to': [
-      'a@gmail.com',
-      'b@gmail.com',
-      'c.gmail.com'
+        'a@gmail.com',
+        'b@gmail.com',
+        'c.gmail.com'
       ],
       'body': 'Please verify',
       'date': '2017-08-20T14:30Z',
