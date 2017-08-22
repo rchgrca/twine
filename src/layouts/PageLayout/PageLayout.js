00992385f8930e5a14f95010d9664de9cf85bf32
Rich Garcia <rchgrca@yahoo.com>
2017-08-21 18:39:46 -0700

    fixes lint errors

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -9,7 +9,7 @@ export const PageLayout = ({ children }) => (
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
-      {' · '}
+    {' · '}
     <Link to='/email' activeClassName='page-layout__nav-item--active'>Email</Link>
 
     <div className='page-layout__viewport'>
