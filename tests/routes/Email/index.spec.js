9c388c435f182360ededc784e149f5f0bc000a69
Rich Garcia <rchgrca@yahoo.com>
2017-08-23 16:53:04 -0700

    removes test for path email

--- tests/routes/Email/index.spec.js
+++ tests/routes/Email/index.spec.js
@@ -4,8 +4,4 @@ describe('(Route) Email', () => {
   it('returns a route configuration object', () => {
     expect(typeof EmailRoute({})).to.equal('object')
   })
-
-  it('has a path \'email\'', () => {
-    expect(EmailRoute({}).path).to.equal('email')
-  })
 })
