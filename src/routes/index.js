fd8b22ff96657588aefca61d9337d267b7accb16
Rich Garcia <rchgrca@yahoo.com>
2017-08-24 09:03:48 -0700

    removes Home, Counter code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/PageLayout/PageLayout'
-import Home from './Home'
-import CounterRoute from './Counter'
 import EmailRoute from './Email'
 
 /*  Note: Instead of using JSX, we recommend using react-router
