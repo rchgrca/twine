08e23c1048cf4707d2502b2ff319355a15e0a2f6
Rich Garcia <rchgrca@yahoo.com>
2017-08-23 20:00:11 -0700

    more styling

--- src/routes/Email/components/MailRead.js
+++ src/routes/Email/components/MailRead.js
@@ -12,7 +12,7 @@ const getMessages = (messages, handleClick) => {
     return (
       <li className='message clearfix' key={`message-read-${i}`}>
         <div className='cell containerBtn'>
-          <button className='btn btn-primary' onClick={() => handleClick(message.id)}>Mark as Unread</button>
+          <button className='btn btn-primary' onClick={() => handleClick(message.id)}>Mark Unread</button>
         </div>
         <div className='cell containerMessage'>
           <div className='subject'>{message.subject}</div>
