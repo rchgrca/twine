da50722c82b8c4da861c635b7a9cc71b982bfa07
Rich Garcia <rchgrca@yahoo.com>
2017-09-07 09:21:06 -0700

    removes common functions from component, puts in methods file

--- src/routes/Email/components/Email.js
+++ src/routes/Email/components/Email.js
@@ -1,39 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
 import MailRead from './MailRead'
 import MailUnread from './MailUnread'
+import { keysrt, convertDateToMs, convertDateToFormat, getMail } from '../modules/methods'
 import './EmailView.scss'
-
-const keysrt = (key) => {
-  return (a, b) => {
-    if (a[key] > b[key]) return 1
-    if (a[key] < b[key]) return -1
-    return 0
-  }
-}
-
-const convertDateToMs = (messages) => {
-  return messages.map((message) => {
-    message['date'] = parseInt(moment(message.date).format('x'), 10)
-    return message
-  })
-}
-
-const convertDateToFormat = (messages) => {
-  return messages.map((message) => {
-    const isMessageToday = moment().format('MMM D, YYYY') === moment(message.date).format('MMM D, YYYY')
-    const sFormat = isMessageToday ? 'h mm A' : 'MMM D, YYYY'
-    message['date'] = moment(message.date).format(sFormat)
-    return message
-  })
-}
-
-const getMail = (messages, isUnread) => {
-  return messages.filter((message) => {
-    return message.unread === isUnread
-  })
-}
 
 export const Email = ({ messages, markRead, markUnread }) => {
   convertDateToMs(messages)
--- src/routes/Email/modules/methods.js
+++ src/routes/Email/modules/methods.js
@@ -1,3 +1,35 @@
+import moment from 'moment'
+
+export const keysrt = (key) => {
+  return (a, b) => {
+    if (a[key] > b[key]) return 1
+    if (a[key] < b[key]) return -1
+    return 0
+  }
+}
+
+export const convertDateToMs = (messages) => {
+  return messages.map((message) => {
+    message['date'] = parseInt(moment(message.date).format('x'), 10)
+    return message
+  })
+}
+
+export const convertDateToFormat = (messages) => {
+  return messages.map((message) => {
+    const isMessageToday = moment().format('MMM D, YYYY') === moment(message.date).format('MMM D, YYYY')
+    const sFormat = isMessageToday ? 'h mm A' : 'MMM D, YYYY'
+    message['date'] = moment(message.date).format(sFormat)
+    return message
+  })
+}
+
+export const getMail = (messages, isUnread) => {
+  return messages.filter((message) => {
+    return message.unread === isUnread
+  })
+}
+
 export const setMultiLineTruncate = (text, maxlength) => {
   // uses JS solution because pure CSS solutions are not cross-browser reliable
   const ellipsis = text.length > maxlength ? '...' : ''
