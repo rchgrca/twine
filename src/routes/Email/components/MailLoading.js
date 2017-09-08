f55e43c59c38110673de6138f569b855231ca13a
Rich Garcia <rchgrca@yahoo.com>
2017-09-07 20:19:46 -0700

    updates correct component name

--- src/routes/Email/components/MailLoading.js
+++ src/routes/Email/components/MailLoading.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const MailEmpty = ({ text }) => {
+export const MailLoading = () => {
   return <img className='loading' src='https://solo.vn/assets/images/cards/loader_blue.gif' />
 }
 
-MailEmpty.propTypes = {
-  text: PropTypes.string.isRequired
-}
-
-export default MailEmpty
+export default MailLoading
