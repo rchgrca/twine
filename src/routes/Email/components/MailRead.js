8a864ebdc67026b267d4cb0dd12b27f6cc4314ee
Rich Garcia <rchgrca@yahoo.com>
2017-09-10 14:37:11 -0700

    restyles web version of read mail

--- src/routes/Email/components/MailRead.js
+++ src/routes/Email/components/MailRead.js
@@ -8,17 +8,17 @@ const getMessages = (messages, handleClick, handleDelete) => {
     return messages.map((message, i) => {
       return (
         <li className='message bg-white mb1 pv3 ph3 ba b--moon-gray br2 cf' key={`message-read-${i}`}>
-          <div className='cell containerMessage'>
+          <div className='cell containerMessage relative-l fr-l w-90-l ph3-l pt0-l'>
             <div className='subject b tl'>{message.subject}</div>
             <div className='tl'>{message.to.join(', ')}</div>
             <div className='containerBody mt3 tl'>{setMultiLineTruncate(message.body, 220)}</div>
-            <div className='containerTimestamp tl'>
+            <div className='containerTimestamp absolute-l top-0-l right-0-l tl tr-l'>
               <div className='cell timestamp moon-gray'>{message.date}</div>
               <div className='cell delete blue pointer' onClick={() => handleDelete(message.id)}>Delete</div>
             </div>
           </div>
           <div className='cell containerBtn'>
-            <button className='br2 ph3 pv2 mt3 bg-orange white ba b--orange ws-normal pointer tl' onClick={() => handleClick(message.id)}>Mark Unread</button>
+            <button className='br2 ph3 pv2 mt3 mt0-l bg-orange white ba b--orange ws-normal pointer tl' onClick={() => handleClick(message.id)}>Mark Unread</button>
           </div>
         </li>
       )
@@ -35,7 +35,7 @@ export const MailRead = ({ messages, handleClick, handleDelete }) => {
     <div className='containerMailRead cf'>
       <h5 className='mb0 pa3 pt0 blue'>READ EMAIL</h5>
       <div className='containerMessages pl1 pr1'>
-        <ul className='list pa0 cf'>
+        <ul className='list ph4-l pa0 cf'>
           {getMessages(messages, handleClick, handleDelete)}
         </ul>
       </div>
