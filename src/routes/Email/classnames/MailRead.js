b182fa845168fea4eaff994578b42c0fcdb109d4
Rich Garcia <rchgrca@yahoo.com>
2017-09-10 17:57:44 -0700

    margin-bottom issues

--- src/routes/Email/classnames/MailRead.js
+++ src/routes/Email/classnames/MailRead.js
@@ -3,7 +3,7 @@ export const MailReadH5 = 'mb0 pa3 pt0 blue'
 export const MailReadContainerMessages = 'ph1'
 export const MailReadUl = 'list ph4-l pa0 cf'
 
-export const MailReadMessageLi = 'bg-white mb1 pv3 ph3 ba b--moon-gray br2 cf'
+export const MailReadMessageLi = 'bg-white mb1 mb4-l pv3 ph3 ba b--moon-gray br2 cf'
 export const MailReadButton = 'br2 ph3 pv2 mt3 mt0-l bg-orange white ba b--orange ws-normal pointer tl'
 export const MailReadContainerMessage = 'relative-l fr-l w-90-l ph3-l pt0-l'
 export const MailReadSubject = 'b tl'
--- src/routes/Email/classnames/MailUnread.js
+++ src/routes/Email/classnames/MailUnread.js
@@ -3,7 +3,7 @@ export const MailUnreadH5 = 'mb0 pa3 blue'
 export const MailUnreadContainerMessages = 'ph1'
 export const MailUnreadUl = 'list ph4-l pa0 cf'
 
-export const MailUnreadMessageLi = 'fl w-48-l bg-white mb1 pa3 ba b--moon-gray br2 cf mb2-l'
+export const MailUnreadMessageLi = 'fl w-48-l bg-white mb1 mb4-l pa3 ba b--moon-gray br2 cf mb2-l'
 export const MailUnreadButton = 'br2 ph3 pv2 mt3 bg-blue white ba b--blue ws-normal pointer tl pointer'
 export const MailUnreadContainerSubject = 'pb2 bb b--moon-gray cf'
 export const MailUnreadSubject = 'b tl'
